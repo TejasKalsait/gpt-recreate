4321e7929623aa54e0cdfdbdb473cdb073047d6b
TejasKalsait <kalsaittejas10@gmail.com>
2024-07-24 00:47:06 -0400

    Finished coding for Distributed processing

--- notes/NOTES.md
+++ notes/NOTES.md
@@ -1,7 +1,141 @@
 # Recreating GPT-2
 
+## Basics and starter code
+
 ![transformer architecture](../images/attention_block.png)
 
 - We recreate the model arch from hugging face
 - The position encodings are a sinosudal wave. We can extract a particular channel value from position encodings and plot it's value across block_size (How a particular dim in the embedding change with context length)
-- 
+
+- use @dataclass decorator for a Data class like a config class.
+
+- torch.view() needs the tensors to be in continuous memory. Some operations, including transpose, breaks this continuity. Then you could use `.contingous().view()` or `.reshape()`
+
+![seld-attention-image](../images/self-attention-arch.jpg)
+
+- We start with the `GPT class` most high level. Then implement the `Block` class that goes inside it, Then finally the `MLP` and `SelfAttention`.
+
+- Loading a model from hugging face and converting it into a `state_dict()` and then viewing and replicating the model is easier.
+
+- When we get the logits and extract the last token's logits [B, vocab_size], and do a softmax to sample, before sampling from this probability, we wanna pick out top `k` probabilities from this distribution and then only sample from then which negates the chance of sampling a very rare token.
+
+- A `nn.ModuleDict()` has a dict() inside with name(key) and nn.Module()(value). Similarly a `nn.ModuleList()` has a list of nn.Modules and indexed by integer
+
+- Gpt-2 tokenizer compression ratio is roughly 3:1. 1/3rds the input.
+
+- `model.to(device)` happens in-place. However, `tensor.to(device)` does not. And it returns a new tensor.
+
+
+## Always overfit a single batch of input first
+
+## Create a data-loader now to give you a fresh batch each time
+
+## Sharing Token embedding and output embedding layers!
+
+- In the embeddings matrix, similar simantic words have very similar embeddings. And the same is true for the output layer that gives probabilities. (lm_head)
+- We want these to give high probabbilities to similar semantic words. We could use the same matrix as recommended by Attention paper.
+
+- It is a very costly layer and it saves lot of computation and gives better results.
+
+# Maintaining of std = 1.0 for layer that get merges from skip connections
+
+- Since we add values from residual connections, the std is not 1.0 anymore. It increases, therefore, to compensate, they recommend initializing the std of these layers by `(1.0/n**0.5)` where n is the number of layers of residual merges.
+In out case n will be 2*n_layers since in each block we do two merges and we have n_layers blocks. So we set c_prop layer to have a low std.
+
+
+## Speed Up!!
+
+![nvidia-precision](../images/nvidia-precision.png)
+
+- FP -> floating point precision
+- TF -> Tensor float
+
+- By default the tensors have a dtype of float32 and means each value takes 32 bits
+- So if a FP32 matrix multiplied by FP32 matrix, the output type is FP32 but an NVIDIA GPU will convert that internally to TF32 to perform calculations much faster with a cost of answers being marginally approximate which is negligible but 8x more speed in A100.
+Basically crops out some precision
+`torch.set_float32_matmul_precision('high)`
+- A good measure is tokens per second btw
+- Bfloat16 offers even more speed with lesser precision. 
+- To convert to bfloat 32, only add this to forward pass, and nothing in backward pass.
+
+bash
+'''
+with torch.autocast(device_type = device, dtype = bfloat16):
+logits, loss = model(x, y)
+'''
+
+## Torch Compile
+
+![nvidia-gpu-SMs](../images/nvidia-gpu-kernal.png)
+
+`torch.compile` -> It's a compilier similar to gcc but for neural networks.
+
+- It reduces Python overhead and GPU read/writes.
+
+- It sees the Entire model and knows what kind of operations we are going to perform. And it removes the python interpreter which goes line by line and instead compiles the whole thing and optimizes it based on knowing every operation.
+
+- Reduces many round trips from GPU kernal to GPU memory to a single round trip. (This trip has a bandwidth which is costly). `Also called kernal fusion`.
+
+# FuseAttention (2022)
+
+- The torch.compile does not work the best with the attention block where we do [T, T] matrix calculation. FuseAttention uues the fusion of GPU kernals where they have L3 RAM to only perform attention more efficiently. The entire attention matrix is never materialized and they calculate a softmax on the run itself. (Instead of after getting all the values). This was the GPU makes only 1 round trip to HMB memory. Even tho this operation performs more Terraflops of operations, it is roughly 7x faster because all of it is happening inside the kernal itself which is very fast.
+
+- `F.scaled_dot_product_attention(q, k, v, is_casual = True)` When we use this attention implementation torch.compile() will call the Flash attention and this will run on fused kernal. No idea why it does not call flashattention when we write our own attention block.
+
+## Cosine Decay learning rate schedule with warmup
+
+![cosine-learning-rate decay](../images/cosine_learningrate.png)
+
+- Increases linearly, then decreases with a cosine curve, until a minimum value.
+
+## Gradient Accumulation
+
+- If the batch_size we want is too high for our tiny GPU, we can simulate the effects of the higher batch_size by accumulating the gradients and running longer.
+- For example, we want to simulate a batch size of 524,288 batches and tokens (B*T) at once.
+- We can have a micro batch and token size of say B = 16, T = 1024.
+- We make sure that this bigger number is perfectle divisible by this micro batch.
+- We take this [B, T] and feedforward, we calculate the gradients but don't update the parameters. The we take another [B, T] and feedward this micro batch and accumulate the gradients and only update the parameters when entire big batch is simulated.
+
+- A bug in the gradient accumulation is the following - 
+
+Suppose the loss is a mean squared error loss for a toy example where logits [4, 1] and y [4,]
+
+loss = [1/4 *
+            (logits[0, 0], y[0]),
+            (logits[1, 0], y[1]),
+            (logits[2, 0], y[2]),
+            (logits[3, 0], y[3]),
+]
+This is the simple mean squared error loss
+
+Now if we pass a batch of 4 like above, the loss will be averaged (1/4).
+If we pass single example as micro batches and 4 steps of accumulating and updating, we `completely negate the mean part of the equation`. So Loss accumulation for a mean-kinda loss function is basically just summing it up.
+To compensate for this, we need to divide the loss by 4 or num_of_steps accumulating basically to account for the mean.
+
+## Distributed Data Parallel (DDP)
+
+- If we have 8 GPUs, we can run 8 processes and each process will run on one GPU.
+- They will process different parts and in the end we average kinda merge them.
+- The processes are launched not via normal python run now but via torch.run which runs 8 processes in parallel.
+
+- Check the DDP implemetation code in gpt_2.py
+
+-   ddp_rank = int(os.environ['RANK'])                  # Unique ID for each process of each node
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])      # Unique ID for each process within a node (Used for setting GPU number)
+    ddp_world_size = int(os.environ['WORLD_SIZE'])      # Number of processes participating
+
+- `torchrun --standalone --nproc_per_node 8 gpt2.py`
+
+- This launches 8 processes with unique ranks all running at the same time
+- We need to make changes to some code to know now we are running multiple processes
+
+- Like grad_accum_steps now is `[B, T, #processes]`
+
+- The data is shared between the GPU's and this the data loading, (getting a batch should not be same for all and we add a clever offset based on which process it is)
+
+- The `DistributedDataParallel` class wraps the model in a DDP class which makes no change in the forward pass, but during backward pass, when the gradients from each process is accumulated, it averages out all gradients from each process and populates the average in each process's gradients.
+Pretty neeeeeet!!!!!
+
+- The DDP() will do an all_reduce() after backpropagation that will propagate the average gradients too all processes. This is done when loss.backward() is called.
+
+- In our code, this sync will happen in every iteration of micro_batch calculation because we do forward and backward for every micro bitch and later take optimization step. We want to sync only after the entire micro batches gradients have been populated. Then we once do a all_reduce() and take a step of optimization.
--- src/gpt2.py
+++ src/gpt2.py
@@ -1,13 +1,40 @@
 from dataclasses import dataclass
+
 import math
+
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+from torch.distributed import init_process_group, destroy_process_group
+
 import tiktoken
+
+import os
+import time
+import inspect
+import sys
+
+# Parameters
+relative_input_path = '../data/shakespear.txt'
+
+total_batch_size = 524288       # in terms of tokens(B, T) 2**19
+#total_batch_size = 524288 // 4
+
+micro_batch_size = 4
+micro_block_size = 512
+
+max_lr = 6e-4
+min_lr = 0.1 * max_lr
+warmup_epochs = 10
+
+epochs = 50
 
 # Class to store the model parameters
 @dataclass
 class GPTConfig:
+    batch_size : int = 16
     block_size : int= 1024      # Context length
     vocab_size : int = 50257    # 50000 merges, 256 binary, 1 |<endoftext>|
     n_layer : int = 12
@@ -25,6 +52,8 @@ class CasualSelfAttention(nn.Module):
         self.n_embed = config.n_embed
         self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)     # Concatenated key, query, value
         self.c_proj = nn.Linear(config.n_embed, config.n_embed)         # projection because of skip connection
+        # flag to indicate this layer should be scaled to maintain std 1.0 after residual connection
+        self.c_proj.RESIDUAL_SCALE = 1
         mask = torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size)
         self.register_buffer("bias", mask)
 
@@ -38,12 +67,20 @@ class CasualSelfAttention(nn.Module):
         k = k.contiguous().view(B, T, self.n_head, C // self.n_head).transpose(1, 2)        # All these have shapes [B, num_head, T, head_size]
         q = k.contiguous().view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = k.contiguous().view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        # Attention of tokens calculation
-        attention : torch.Tensor = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))    # [B, nh, T, hs] x [B, nh, hs, T]  --> [B, nh, T, T]
-        # Masking the attention since we are working on a decoder and also applying the softmax
-        attention = attention.masked_fill(self.bias[:, :, T, T] == 0, float('-inf'))
-        attention = F.softmax(attention, dim = -1)
-        y : torch.Tensor = attention @ v        # [B, nh, T, T] x [B, nh, T, hs]  --> [B, nh, T, hs]
+        
+        
+        # Attention of tokens calculation using traditional method. We are used to this and
+        # torch.compile does not call fuse attention on this
+        #
+        # attention : torch.Tensor = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))    # [B, nh, T, hs] x [B, nh, hs, T]  --> [B, nh, T, T]
+        # # Masking the attention since we are working on a decoder and also applying the softmax
+        # attention = attention.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        # attention = F.softmax(attention, dim = -1)
+        # y : torch.Tensor = attention @ v        # [B, nh, T, T] x [B, nh, T, hs]  --> [B, nh, T, hs]
+
+        # Implementing Fuse attention block instead
+        y = F.scaled_dot_product_attention(q, k, v, is_causal = True)
+
         # y is of shape [B, nh, T, hs] and we need it again in [B, T, C] form
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         # Projection layer for skip connection shape match
@@ -59,6 +96,8 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embed, 4 * config.n_embed)
         self.gelu = nn.GELU(approximate = 'tanh')
         self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
+        # flag to indicate this layer should be scaled to maintain std 1.0 after residual connection
+        self.c_proj.RESIDUAL_SCALE = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -96,7 +135,25 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias = False)
 
-    def forward(self, x):
+        # weight sharing scheme that saves computation (recommended in paper)
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # Apply some function by passing each nn.Module one by one
+        self.apply(self._init_weight)
+
+    # Weight initializer
+    def _init_weight(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'RESIDUAL_SCALE'):
+                std = (2 * self.config.n_layer) ** -0.5
+            nn.init.normal_(module.weight, mean = 0.0, std = std)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean = 0.0, std = 0.02)
+
+    def forward(self, x, y = None):
 
         # Input data size
         B, T = x.size()
@@ -114,28 +171,237 @@ class GPT(nn.Module):
         # Forwarding the final layer
         x = self.transformer.ln_f(x)                                    # [B, T, C]
         logits = self.lm_head(x)                                        # [B, T, vocab_size]
-        
-        return logits
-    
 
+        # Calculating loss
+        loss = None
+        if y is not None:
+            # cross entropy expect x shape [B, T] and y shape [B*T,]
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)) ,y.view(-1))
+        
+        return logits, loss
+    
+    def configure_optimizer(self, weight_decay, learning_rate, device):
+
+        # Collecting all the parameters that require gradients
+        param_dict = {pn : p for pn, p in self.named_parameters()}
+        param_dict = {pn : p for pn, p in param_dict.items() if p.requires_grad}
+
+        # All 2D and above parameters will require weight decay and other 1D parameters like
+        # Layer norm, GELU, won't so seperate them out
+        decay_params = [p for pn, p in param_dict.items() if p.dim() >= 2]
+        nondecay_params = [p for pn, p in param_dict.items() if p.dim() < 2]
+
+        # Two seperate groups of parameters
+        optim_params = [
+            {'params': decay_params, 'weight_decay' : weight_decay},
+            {'params': nondecay_params, 'weight_decay' : 0.0}
+        ]
+
+        # Parameter Stats
+        num_decay_param = sum(p.nelement() for p in decay_params)
+        num_nondecay_param = sum(p.nelement() for p in nondecay_params)
+        if master_process:
+            print(f"Total Decay parameters --> {num_decay_param}")
+            print(f"Total Non-Decay parameters --> {num_nondecay_param}")
+
+        # Checking if the fused kernal option is available, if yes we want to make an optimizer that uses it
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device
+        if master_process:
+            print(f"Using fused AdamW --> {use_fused}")
+            print("_________________________________________________________________________")
+
+        # Initialize optimizer and return
+        optimizer = torch.optim.AdamW(optim_params, lr = learning_rate, betas = (0.9, 0.95), eps = 1e-8, fused = use_fused)
+        return optimizer
+
+# Creating a data loader that retuns a fresh batch of x, y
+class DataLoader:
+
+    def __init__(self, B, T, input_file, process_rank, num_processes) -> None:
+        # saving shapes
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
+        # Open file and save data at initialization
+        with open(input_file, 'r') as f:
+            text = f.read()
+        # encode text and create tokens
+        encoder = tiktoken.get_encoding("gpt2")
+        tokens = encoder.encode(text)
+        self.tokens = torch.tensor(tokens)
+        if master_process:
+            # stats
+            print(f"Loaded data. Total {len(self.tokens)} tokens available")
+            print(f"1 epoch consists of {len(self.tokens) // (B * T)} major batches")
+
+        # From which index to sample the batch from (depends on which process this is)
+        self._start = self.B * self.T * self.process_rank
+
+    def next_batch(self):
+
+        B, T = self.B, self.T   # batch size and context size
+        # index from startin point to start + batch*tokens + 1
+        # +1 because we need to save that as y's last value
+        buffer = self.tokens[self._start:(B * T) + self._start + 1]
+        # Build x and y 
+        x = buffer[:-1].view(B, T)      # [B, T]
+        y = buffer[1:].view(B, T)       # [B, T]
+        # Move the start to next batch start
+        self._start += (B * T * self.num_processes)
+        # In case next batch sampling will go out of bounds, reset to zero
+        if self._start + (B * T + 1) > len(self.tokens):
+            self._start = self.B * self.T * self.process_rank
+
+        return x, y
 # ________________________________________________________________________________________________________________
 
-# Hyperparameters
-num_returned_sequences = 5
-max_length = 75
+# Cosine decay learning rate scheduler
+def get_lr(epoch):
+    
+    # Linear warmup going on
+    if epoch < warmup_epochs:
+        return max_lr * (epoch+1) / warmup_epochs
+    # If epoch > total epochs (always return min possible then)
+    if epoch > epochs:
+        return min_lr
+    # In between, cosine decay
+    decay_ratio = (epoch - warmup_epochs) / (epochs - warmup_epochs)
+    assert 0 <= decay_ratio <= 1,0
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print("Training on -->", device)
+# Set up DDP (Distributed Data Parallel)
+ddp = int(os.environ.get('RANK', -1)) != -1     # Bool if ddp is present
+if ddp:
+    # Setup ddp
+    assert torch.cuda.is_available(), "DDP requires CUDA"
+    init_process_group(backend='nccl')
+    ddp_rank = int(os.environ['RANK'])                  # Unique ID for each process of each node
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])      # Unique ID for each process within a node (Used for setting GPU number)
+    ddp_world_size = int(os.environ['WORLD_SIZE'])      # Number of processes participating
+    device = f'cuda:{ddp_local_rank}'                   # Setting which GPU
+    torch.cuda.set_device(device)
+    master_process = ddp_rank == 0                      # One master process for prints
+else:
+    # vanilla
+    ddp_rank = 0
+    ddp_local_rank = 0
+    ddp_world_size = 1
+    master_process = True
+    # Setting device
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
 
-model = GPT(GPTConfig())
-model = model.to(device)    # Switching to GPU
+# Autocast device type should be 'cuda' and not 'cuda:0'
+autocast_device = 'cuda'
 
-# Creating tokens
-encoder = tiktoken.get_encoding("gpt2")
-tokens = encoder.encode("Hi. Let me tell you a story about the planet Pluto. Once,")
-tokens = torch.tensor(tokens, dtype = torch.long)   # [T,]
-tokens = tokens.unsqueeze(0).repeat(num_returned_sequences, 1)  # [B, T]
-x = tokens.to(device)
+# print training on what device
+if master_process:
+    print("Training on -->", device)
+
+# File Paths
+script_dir = os.path.dirname(os.path.abspath(__file__))
+input_file_path = os.path.join(script_dir, relative_input_path)
+
+# Setting the precision for speed to take advantage of Nvidia TF32
+torch.set_float32_matmul_precision('high')  # Default highest
+
+# Setting seeds
+torch.manual_seed(1010)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1010)
+
+# Gradient Accumulation to simulate bigger batch sizes
+assert total_batch_size % (micro_batch_size*micro_block_size*ddp_world_size) == 0, f"The total block size {total_batch_size} is not divisible by micro_batch_size * micro_block_size * num_processes {micro_batch_size}*{micro_block_size}*{ddp_world_size}"
+# Accumulate gradients for these many steps
+grad_accum_steps = total_batch_size // (micro_batch_size*micro_block_size*ddp_world_size)
+if master_process:          # Only printed by master process
+    print(f"{grad_accum_steps} micro batches will be processed in one major batch")
+
+# Initializing DataLoader, Model, Optimizer
+data_loader = DataLoader(micro_batch_size, micro_block_size, input_file_path, process_rank = ddp_rank, num_processes = ddp_world_size)
+model = GPT(GPTConfig(vocab_size = 50304))
+model.to(device)
+model = torch.compile(model)
+# This container wraps the model in a DDP class which makes no change in the forward pass, but
+# during backward pass, when the gradients from each process is accumulated, it averages out all gradients
+# from each process and populates the average in each process's gradients
+if ddp:
+    model = DDP(model, device_ids = [ddp_local_rank])
+# Unwrapping the raw model from the DDP wrap to pass to optimizer
+raw_model = model.module if ddp else model
+optimizer = model.configure_optimizer(weight_decay = 0.1, learning_rate = 6e-4, device = device)
+
+
+# _____________________________________________________________________________________________________
+
+# Training Loop
+for epoch in range(epochs):
+    # record time
+    tock = time.time()
+
+    # Zero grad
+    optimizer.zero_grad()
+
+    # To keep track of loss for the batch instead of micro_batch
+    loss_accum = 0.0
+
+    # Gradient accumulation for simulating bigger batches
+    for micro_step in range(grad_accum_steps):
+        # Fetch a new batch
+        x, y = data_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        # Feed forward with dtype = torch.bfloat16 whenever possible (some operations) to speed up matmul
+        # with torch.autocast(device_type = device, dtype = torch.bfloat16):
+        with torch.autocast(device_type = autocast_device, dtype = torch.bfloat16):
+            logits, loss = model(x, y)
+        loss = loss / grad_accum_steps      # To normalize the mean-like loss which is lost when acccumulating (Check NOTES)
+        loss_accum += loss.detach()
+        # Disable sync and only enable it on the last step again so that it syncs gradients only on last step of micro bacth loop
+        if ddp:
+            model.require_backward_grad_sync = (micro_step == grad_accum_steps - 1)
+        # Zero grad and backward pass
+        loss.backward()
+
+    # Enabling sync of losses across all the processes for the master process to print 
+    if ddp:
+        dist.all_reduce(loss_accum, op = dist.ReduceOp.AVG)
+
+    # Clipping the gradient to avoid shock inscase a bad batch comes in.
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+    # Get learning rate based on the learning rate scheduler and use this in optimizer
+    lr = get_lr(epoch)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    # Update step
+    optimizer.step()
+
+    # To have cuda and cpu sync with no pending works
+    torch.cuda.synchronize()
+
+    # record time
+    tick = time.time()
+    dt = (tick - tock)
+
+    # How many tokens processed per sec
+    tokens_processed = data_loader.B * data_loader.T * grad_accum_steps * ddp_world_size
+    tokens_per_sec = tokens_processed / dt
+    # Print Stats
+    if master_process:
+        print(f"Epoch {epoch}/{epochs} | Loss --> {loss_accum:.2f}| Grad norm --> {norm:4f} | LR --> {lr:.4f} | Time --> {dt:.2f} | Tokens/sec --> {tokens_per_sec:.2f}")
+
+# Clean exit of processes
+if ddp:
+    destroy_process_group()
+
+sys.exit(0)
+
+# _____________________________ GENERATE ___________________________________________________________
 
 # Generating Loop
 torch.manual_seed(55)
